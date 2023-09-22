5fb117367511b9e652a9e322862a01864ad864ca
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-09-22 11:21:00 +0200

    working mouse movement

--- player/PlayerCharacter.cs
+++ player/PlayerCharacter.cs
@@ -1,126 +1,149 @@
 using System;
-using System.Globalization;
 using Godot;
 
 namespace shootergame.player;
 
 public partial class PlayerCharacter : CharacterBody3D
 {
-    private float _gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+	[ExportGroup("Movement")]
+	[Export]
+	public float MaxVelocity = 5.0f;
 
-    private Camera3D _camera;
+	[Export]
+	public float MovementForce = 2700.0f;
 
-    [ExportGroup("Movement")]
-    [Export]
-    public float MaxVelocity = 5.0f;
+	[Export]
+	public float Friction = 25.0f;
 
-    [Export]
-    public float MovementForce = 2700.0f;
+	[Export]
+	public float AirFrictionFactor = 0.05f;
 
-    [Export]
-    public float Friction = 25.0f;
+	[ExportSubgroup("Air Control")]
+	[Export]
+	public float AirSpeedControl = 1.0f;
 
-    [Export]
-    public float AirFrictionFactor = 0.05f;
+	[Export]
+	public float AirStrafeControl = 1.0f;
 
-    [ExportSubgroup("Air Control")]
-    [Export]
-    public float AirSpeedControl = 0.3f;
+	[ExportSubgroup("")]
+	[Export]
+	public float TerminalVelocity = 55.0f;
 
-    [Export]
-    public float AirStrafeControl = 0.1f;
+	[Export]
+	public float MassKg = 60.0f;
 
-    [ExportSubgroup("")]
-    [Export]
-    public float TerminalVelocity = 55.0f;
+	[Export]
+	public float JumpForceN = 300.0f;
 
-    [Export]
-    public float MassKg = 60.0f;
+	[ExportGroup("Camera")]
+	[Export]
+	public float LookaroundSpeed = 1.0f;
 
-    [Export]
-    public float JumpForceN = 300.0f;
+	
+	private float _lookaroundSpeedReduction = 0.002f;
 
-    public override void _Ready()
-    {
-        _camera = GetNode<Camera3D>("Camera3D");
-        GD.Print(_camera.Transform.Basis);
-        Input.MouseMode = Input.MouseModeEnum.Captured;
-    }
+	private float _gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
-    public override void _Input(InputEvent @event)
-    {
-        if (@event is InputEventMouseMotion mouseMotion)
-        {
-            // GD.Print(mouseMotion.Relative);
-        }
-    }
+	private Camera3D _camera;
 
-    public override void _Process(double delta)
-    {
-        Velocity = Movement(delta, Gravity(delta, Jump(Velocity)));
-        MoveAndSlide();
+	private Vector2 _viewPoint = new(0.0f,0.0f);
 
-        // GD.Print(
-        // 	$"{Velocity.X.ToString("F", CultureInfo.InvariantCulture)},{Velocity.Y.ToString("F", CultureInfo.InvariantCulture)},{Velocity.Z.ToString("F", CultureInfo.InvariantCulture)}");
-    }
+	private float _currentSpeed = 0.0f;
 
-    private Vector3 Movement(double delta, Vector3 velocity)
-    {
-        var onFloor = IsOnFloor();
+	public override void _Ready()
+	{
+		_camera = GetNode<Camera3D>("Camera3D");
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+	}
 
-        var inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back").Normalized();
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is not InputEventMouseMotion mouseMotion) return;
+		
+		_viewPoint.X += mouseMotion.Relative.X * LookaroundSpeed * _lookaroundSpeedReduction;
+		_viewPoint.Y += mouseMotion.Relative.Y * LookaroundSpeed * _lookaroundSpeedReduction;
+		_viewPoint.Y = (float)Math.Clamp(_viewPoint.Y, -Math.PI/2, Math.PI/2);
 
-        var direction = new Vector3(inputDir.X, 0, inputDir.Y).Normalized();
-        
-        var directionStrength = direction;
-        if (!onFloor)
-        {
-            directionStrength.Z *= AirSpeedControl;
-            directionStrength.X *= AirStrafeControl;
-        }
+		var transform = _camera.Transform;
+		transform.Basis = Basis.Identity;
+		_camera.Transform = transform;
+		_camera.RotateObjectLocal(Vector3.Up, -_viewPoint.X);
+		_camera.RotateObjectLocal(Vector3.Right, -_viewPoint.Y);
+	}
 
-        velocity.X = Acceleration(delta, velocity.X, directionStrength.X, direction.X);
-        velocity.Z = Acceleration(delta, velocity.Z, directionStrength.Z, direction.Z);
+	public override void _Process(double delta)
+	{
+		Velocity = Movement(delta, Gravity(delta, Jump(Velocity)));
+		MoveAndSlide();
+	}
 
-        return velocity;
+	private Vector3 Movement(double delta, Vector3 velocity)
+	{
+		// GD.Print(-_camera.Transform.Basis.Z);
+		var onFloor = IsOnFloor();
 
-        float Acceleration(double d, float v, float strength, float maxRatio)
-        {
-            if (strength != 0.0f)
-            {
-                // accelerate
-                v += strength * (MovementForce - Friction) / MassKg * (float)d;
-                
-                // limit velocity
-                var max = MaxVelocity * Math.Abs(maxRatio);
-                v = Math.Clamp(v, -max, max);
-            }
-            else
-            {
-                // decelerate with friction
-                var friction = onFloor ? Friction : Friction * AirFrictionFactor;
-                v = Mathf.MoveToward(v, 0, friction * (float)d);
-            }
+		var inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back").Normalized();
 
-            return v;
-        }
-    }
+		var camVec = Vector2.Down.Rotated(_viewPoint.X); 
+		var forwardVector = new Vector2(camVec.X, camVec.Y);
+		// using rotation matrix to rotate by 90
+		var sidewaysVector = new Vector2(forwardVector.Y, -forwardVector.X);
+		var forwardDirection = forwardVector * inputDir.Y;
+		var sidewaysDirection = sidewaysVector * inputDir.X;
+		var direction2D = forwardDirection + sidewaysDirection;
+		var direction = new Vector3(direction2D.X, 0, direction2D.Y).Normalized();
+		
+		GD.Print(direction);
 
-    private Vector3 Gravity(double delta, Vector3 velocity)
-    {
-        if (!IsOnFloor())
-            velocity.Y -= _gravity * (float)delta;
+		var directionStrength = direction;
+		if (!onFloor)
+		{
+			directionStrength.Z *= AirSpeedControl;
+			directionStrength.X *= AirStrafeControl;
+		}
 
-        velocity.Y = Math.Clamp(velocity.Y, -TerminalVelocity, float.MaxValue);
+		velocity.X = Acceleration(delta, velocity.X, directionStrength.X, direction.X, onFloor);
+		velocity.Z = Acceleration(delta, velocity.Z, directionStrength.Z, direction.Z, onFloor);
 
-        return velocity;
-    }
+		return velocity;
+	}
 
-    private Vector3 Jump(Vector3 velocity)
-    {
-        if (Input.IsActionJustPressed("jump") && IsOnFloor())
-            velocity.Y = JumpForceN / MassKg;
+	private float Acceleration(double d, float v, float strength, float maxRatio, bool onFloor)
+	{
+		if (strength != 0.0f)
+		{
+			// accelerate
+			v += strength * (MovementForce - Friction) / MassKg * (float)d;
 
-        return velocity;
-    }
+			// limit velocity
+			var max = MaxVelocity * Math.Abs(maxRatio);
+			v = Math.Clamp(v, -max, max);
+		}
+		else
+		{
+			// decelerate with friction
+			var friction = onFloor ? Friction : Friction * AirFrictionFactor;
+			v = Mathf.MoveToward(v, 0, friction * (float)d);
+		}
+
+		return v;
+	}
+
+	private Vector3 Gravity(double delta, Vector3 velocity)
+	{
+		if (!IsOnFloor())
+			velocity.Y -= _gravity * (float)delta;
+
+		velocity.Y = Math.Clamp(velocity.Y, -TerminalVelocity, float.MaxValue);
+
+		return velocity;
+	}
+
+	private Vector3 Jump(Vector3 velocity)
+	{
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+			velocity.Y = JumpForceN / MassKg;
+
+		return velocity;
+	}
 }
