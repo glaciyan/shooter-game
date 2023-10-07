98306940b2d4cbf5f4f02bbbb6a8d80090010855
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-10-07 11:28:19 +0200

    docs

--- player/script/VectorUtil.cs
+++ player/script/VectorUtil.cs
@@ -13,12 +13,22 @@ public static class VectorUtil
     public static Vector3 ToVector3Flat(this Vector2 v, float y = 0f) => new(v.X, y, v.Y);
     public static Vector2 ToVector2Flat(this Vector3 v) => new(v.X, v.Z);
 
+    /// <summary>
+    /// Limits the lenght of the vector to 1.
+    /// </summary>
+    /// <param name="v">Target vector</param>
+    /// <returns>Vector of lenght 1 if lenght was greater than 1, otherwise the same vector.</returns>
     public static Vector3 FastLimit(this Vector3 v)
     {
         var lq = v.LengthSquared();
         return lq > 1f ? v / Mathf.Sqrt(lq) : v;
     }
 
+    /// <summary>
+    /// Limits the lenght of the vector to 1.
+    /// </summary>
+    /// <param name="v">Target vector</param>
+    /// <returns>Vector of lenght 1 if lenght was greater than 1, otherwise the same vector.</returns>
     public static Vector2 FastLimit(this Vector2 v)
     {
         var lq = v.LengthSquared();
